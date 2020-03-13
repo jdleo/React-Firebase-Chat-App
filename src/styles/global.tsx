9da2f2f4556e5dc0af8471ed6a581b03be0623ac
John Leonardo <jdleonardo5@icloud.com>
2020-03-13 12:53:34 -0700

    set up custom fonts and global styles

---
+++ src/styles/global.tsx
@@ -0,0 +1,12 @@
+// interface for styles dictionary
+interface IStyles {
+    [key: string]: any;
+}
+
+// export global styles dictionary
+export const styles: IStyles = {
+    container: {
+        textAlign: 'center',
+        padding: 40
+    }
+};
